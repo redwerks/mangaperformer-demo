59a42d58fe36b6fcc1cb288e8f8ef53af2126ff5
Daniel Friesen <daniel@nadir-seen-fire.com>
2013-11-04 20:07:59 -0800

    Write a grunt task to sync bower.json with package.json.
    The task reads package.json and bower.json and then updates the
    description, version, homepage, author, contributors, and licenses.
    
    This way we only have to maintain this data inside package.json.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,12 +22,53 @@ module.exports = function(grunt) {
 					port: 8888
 				}
 			}
+		},
+
+		watch: {
+			'meta-sync': {
+				files: [
+					'package.json',
+				],
+				tasks: ['meta-sync']
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('server', ['connect:server']);
 	grunt.registerTask('server:public', ['connect:public_server']);
+
+	// Sync metadata from package.json into bower.json
+	grunt.registerTask('meta-sync', 'Sync metadata from package.json into bower.json.', function() {
+		var pkg = grunt.file.readJSON('package.json');
+		var bower = grunt.file.readJSON('bower.json');
+
+		// Description
+		bower.description = pkg.description;
+
+		// Version
+		bower.version = pkg.version;
+
+		// Homepage
+		bower.homepage = pkg.homepage;
+
+		// Author and contributors
+		bower.authors = [pkg.author].concat(pkg.contributors).map(function(c) {
+			var author = {};
+			if ( c.name ) author.name = c.name;
+			if ( c.email )author.email = c.email;
+			if ( c.url ) author.homepage = c.url;
+			return author;
+		});
+
+		// License
+		bower.license = pkg.license.url;
+
+		// Save
+		grunt.file.write('bower.json', JSON.stringify(bower, null, 2) + '\n');
+		grunt.log.writeln('File "bower.json" updated.');
+	});
 
 };
